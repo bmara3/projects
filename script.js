3ababcbe6aa1c217f8a4086142571aecd4a34c62
Marlen <mail.ru>
2020-08-27 15:18:34 +0500

    first app

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello!");
+const numberOfFilms = +prompt('Сколько фильмов ты посмотрел?', '');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+const lastFilm = prompt('Один из последних просмотренных фильмов?');
+const ratingLastFilm = prompt('На сколько оцените его?');
+personalMovieDB.movies.push(`'${lastFilm}': '${ratingLastFilm}'`);
+console.log(personalMovieDB);
