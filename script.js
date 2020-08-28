299f19a8f69ad40eb1bc9cd27fd74d76ed482f67
Marlen <mail.ru>
2020-08-28 14:36:35 +0500

    practic 2

--- script.js
+++ script.js
@@ -6,7 +6,26 @@ const personalMovieDB = {
 	genres: [],
 	privat: false
 };
-const lastFilm = prompt('Один из последних просмотренных фильмов?');
-const ratingLastFilm = prompt('На сколько оцените его?');
-personalMovieDB.movies[lastFilm] =  ratingLastFilm;
+
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', '');
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('Done!');
+	} else {
+		console.log('Error!');
+		i--;
+	}
+}
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+	console.log('Вы киноман!');
+} else {
+	console.log('Произошла ошибка!');
+}
+
 console.log(personalMovieDB);
